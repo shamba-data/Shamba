8aaa4970c1b80a4e43386144217b22021fbdbde4
tony <b.mboya@alustudent.com>
2023-01-01 11:46:56 +0200

    added the under construction page

--- app/src/pages/construction/index.tsx
+++ app/src/pages/construction/index.tsx
@@ -7,10 +7,14 @@ export default function Index() {
       <Nav />
       <section className="flex min-h-screen flex-col items-center justify-center">
         <div className="relative h-[200px] w-[300px]">
-          <BlurImage imageUrl="/404.png" preload={false} rounded={true} />
+          <BlurImage
+            imageUrl="/construction.png"
+            preload={false}
+            rounded={true}
+          />
         </div>
         <p className="mt-[2rem] text-center">
-          The page you are looking for is not found Come back to the home page
+          This page is under construction Come back to the home page
         </p>
         <Link href="/">
           <button className="mt-[14px] cursor-pointer rounded-md bg-green px-4 py-2 font-medium text-white md:py-4 lg:mt-[18px]">
