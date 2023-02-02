fda41eefa8b4d05c5817e11de884ee58ddef2b95
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2023-02-02 10:22:51 +0200

    contact page done- need to add padding  small screen form

--- app/src/pages/contactus.tsx
+++ app/src/pages/contactus.tsx
@@ -1,14 +1,16 @@
 import { Footer, HeadSeo, Nav } from "../components/landingPage";
 import BlurImage from "../components/UI/BlurImage";
 import { GoLocation } from 'react-icons/go'
-import {BsTelephonePlus} from 'react-icons/bs'
-import {HiOutlineMail} from 'react-icons/hi'
+import { BsTelephone, BsLinkedin } from 'react-icons/bs'
+import { HiOutlineMail } from 'react-icons/hi'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { GrFacebook } from 'react-icons/gr'
 const Contact = () => {
   const labelClasses = "font-medium text-lg text-green tracking-wide";
   const inputClasses =
     "border-[1px] rounded-md px-2 py-2 w-[300px] focus:outline-none bg-white mt-2";
   const divClasses = "flex flex-col";
-  const companyInfoClasses = " flex flex-col w-full max-w-lg p-8 rounded-xl shadow-lg"
+  const companyInfoClasses = " flex flex-col md:flex-row w-full max-w-4xl p-8 max-h-[56rem] rounded-xl shadow-lg"
   return (
     <>
       <HeadSeo title="Contact Us" />
@@ -28,7 +30,7 @@ const Contact = () => {
               {/*company contact info*/}
               <div className="flex flex-col space-y-4">
                 <div className="inline-flex space-x-2 items-center">
-                  <BsTelephonePlus className="text-green" size={24} />
+                  <BsTelephone className="text-green" size={24} />
                   <span className="mt-2">260978964998</span>
                 </div>
                 <div className="inline-flex space-x-2 items-center">
@@ -39,52 +41,64 @@ const Contact = () => {
                   <GoLocation className="text-green" size={24} />
                   <span className="mt-2">Plot 3870 Kafue Drive, Riverside, Kitwe</span>
                 </div>
-                
               </div>
-
+              {/*social media*/}
+              <div className=" flex space-x-4">
+                <a href="#">
+                  <GrFacebook className="text-blue-800" size={31} />
+                </a>
+                <a href="https://www.linkedin.com/company/shamba-data/?viewAsMember=true">
+                  <BsLinkedin className="text-blue-700" size={32} />
+                </a>
+                <a href="https://twitter.com/shambaData">
+                  <AiOutlineTwitter className="text-cyan-500" size={36} />
+                </a>
+              </div>
             </div>
             {/*contact us form */}
-            <form className="mt-5 flex flex-col gap-5">
-              <div className={divClasses}>
-                <label className={labelClasses}>Your Email</label>
-                <input
-                  type="email"
-                  placeholder="aimebah@gogle.com"
-                  className={inputClasses}
-                  name="email"
-                  required
-                />
-              </div>
-              <div className={divClasses}>
-                <label className={labelClasses}>Your Topic</label>
-                <input
-                  type="text"
-                  placeholder="E.g cancel my subscription"
-                  className={inputClasses}
-                  name="topic"
-                  required
-                />
-              </div>
+            <div className="rounded-xl">
+              <form className="mt-5 flex flex-col gap-5">
+                <div className={divClasses}>
+                  <label className={labelClasses}>Your Email</label>
+                  <input
+                    type="email"
+                    placeholder="aimebah@gmail.com"
+                    className={inputClasses}
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className={divClasses}>
+                  <label className={labelClasses}>Your Topic</label>
+                  <input
+                    type="text"
+                    placeholder="E.g cancel my subscription"
+                    className={inputClasses}
+                    name="topic"
+                    required
+                  />
+                </div>
 
-              <div className={divClasses}>
-                <label className={labelClasses}>Your Message</label>
-                <textarea
-                  className="m-0 block w-[300px] rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
-                  rows={3}
-                  placeholder="Your message"
-                ></textarea>
-              </div>
+                <div className={divClasses}>
+                  <label className={labelClasses}>Your Message</label>
+                  <textarea
+                    className="m-0 block w-[300px] rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+                    rows={3}
+                    placeholder="Your message"
+                  ></textarea>
+                </div>
 
-              <button
-                type="submit"
-                className="w-[90px] rounded-md bg-green py-3 font-medium tracking-wide text-white xl:w-[100px] xl:text-lg"
-              >
-                Submit
-              </button>
-            </form>
+                <button
+                  type="submit"
+                  className="w-[90px] rounded-md bg-green py-3 font-medium tracking-wide text-white xl:w-[100px] xl:text-lg"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
-
-          <div className="relative hidden h-[600px] w-[490px] md:block">
+          {/*image */}
+          <div className="relative hidden h-[490px] w-[490px] md:block">
             <BlurImage
               imageUrl="/contact.png"
               preload={false}
