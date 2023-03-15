47cf65ad1415a66c69a789f84e1cbb77e713e1bd
BrightonMboya <b.mboya@alustudent.com>
2023-03-15 10:22:20 +0200

    switching to a new origin/main

---
+++ node_modules/.pnpm/@trpc+react-query@10.4.3_x72ucuwg27nhluiflrpycjcdya/node_modules/@trpc/react-query/dist/getArrayQueryKey-24927615.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * To allow easy interactions with groups of related queries, such as
+ * invalidating all queries of a router, we use an array as the path when
+ * storing in tanstack query. This function converts from the `.` separated
+ * path passed around internally by both the legacy and proxy implementation.
+ * https://github.com/trpc/trpc/issues/2611
+ */ function getArrayQueryKey(queryKey, type) {
+    const queryKeyArrayed = Array.isArray(queryKey) ? queryKey : [
+        queryKey
+    ];
+    const [path, input] = queryKeyArrayed;
+    const arrayPath = typeof path !== 'string' || path === '' ? [] : path.split('.');
+    // Construct a query key that is easy to destructure and flexible for
+    // partial selecting etc.
+    // https://github.com/trpc/trpc/issues/3128
+    return [
+        arrayPath,
+        {
+            ...input && {
+                input: input
+            },
+            ...type && type !== 'any' && {
+                type: type
+            }
+        }
+    ];
+}
+
+exports.getArrayQueryKey = getArrayQueryKey;
